d27bc2fa0e1e510824205ab15e3c853aaeea1f76
ninest <parth.kabra@gmail.com>
2020-12-19 12:47:54 +0800

    Initial project scaffold

--- gi/__main__.py
+++ gi/__main__.py
@@ -1,11 +1,26 @@
-# import sys
-# import webbrowser
+import sys
+import webbrowser
 
-from .config import GITIGNORE_FILE_PATH
+# from .config import GITIGNORE_FILE_PATH
 
 
 def main():
-    print(GITIGNORE_FILE_PATH)
+    if len(sys.argv) > 1:
+        command = sys.argv[1].lower()
+        args = sys.argv[2:]
+        run(command, args)
+    else:
+        print("Help text")
+
+
+def run(command, args):
+    if command == "help":
+        print("help text")
+    elif command in ["add", "a"]:
+        print("add")
+        print(args)
+    elif command in ["source", "github"]:
+        webbrowser.open("https://github.com/ninest/gi/")
 
 
 if __name__ == "__main__":
